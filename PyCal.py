597150e92bdc9b34e06db7f399e9909e9a410fb5
klokkanelva <david@vilhelmsson.net>
2015-07-22 22:29:25 +0200

    added a little user input code

--- PyCal.py
+++ PyCal.py
@@ -7,13 +7,20 @@ user_input = input("Enter date for event\n") # right now only works when enterin
 user_input_date = user_input.split("/") # splits user input into a list
 user_input_day = user_input[0]
 user_input_month = user_input[1]
-# user_input_year should be something like:
-# if not user_input.endswith("****"):
-#   user_input_year = "2015"
-# meaning, if the user doesn't enter a year, we assume the event
-# takes place in the current year
+if len(user_input) <= 5:
+    user_input_year = "2015"            # meaning, if the user doesn't enter a year,
+                                        # we assume the event takes place in the current year
+else:
+    user_input_year = user_input[-4:]   # otherwise, we take the last 4 characters of the entered
+                                        # input and assigns it to the year
 
-example_calendar = {"2015":{'Month':'January':{1:{'Day':'monday''Events':None'Etc':None}}}}
+example_calendar = {"Year":user_input_year{'Month':'January':{1:{'Day':'monday''Events':None'Etc':None}}}}
+# ^^^ OK so we're getting a syntax error on the above code
+# maybe it'd be better to do a list of tuples instead? Example below:
+example_calendar = [("Year":user_input_year), ("Month":"user_input_month"), ("Day":user_input_day), ("Events":None), ("Etc":None)]
+
+
+
 
 # idea for dictionary generation
 # {d: {'day': day_dic[datetime.day(year, month, d]}, 'events': ' '} for d in range(1,32)}
