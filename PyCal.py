416a3e64d17e1e2ccf9a6d55a78eb668a1f24fed
klokkanelva <david@vilhelmsson.net>
2015-07-22 21:53:26 +0200

    added sample code

--- PyCal.py
+++ PyCal.py
@@ -1,3 +1,14 @@
 import datetime
+from termcolor import colored, cprint
 
 week_dict = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
+
+example_calendar = {'January':
+                   {1:
+                   {'Day':'monday'
+                   'Events':None
+                   'Etc':None
+                   }
+                   }
+
+                   
