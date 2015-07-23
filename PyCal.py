c8e2b7e35612a2f3fa588331b1f977d6c828c091
klokkanelva <david@vilhelmsson.net>
2015-07-23 11:35:54 +0200

    added variable for today and current year

--- PyCal.py
+++ PyCal.py
@@ -1,5 +1,8 @@
-import datetime
+from datetime import datetime
 from termcolor import colored, cprint
+
+today = datetime.today()                # sets today to a variable we can call
+current_year = today.year               # sets the variable for the current year
 
 week_dict = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
 
@@ -9,7 +12,7 @@ user_input_date = user_input.split("/") # splits user input into a list
 user_input_day = int(user_input_date[0])
 user_input_month = int(user_input_date[1])
 if len(user_input) <= 5:
-    user_input_year = 2015           # meaning, if the user doesn't enter a year,
+    user_input_year = current_year      # meaning, if the user doesn't enter a year,
                                         # we assume the event takes place in the current year
 else:
     user_input_year = user_input[-4:]   # otherwise, we take the last 4 characters of the entered
